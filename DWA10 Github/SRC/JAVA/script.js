962d6a03fc04da4cce25695f63b01dd6f357d7c5
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-06-26 01:30:17 +0200

    Added DWA10 and applied shoelace styling.

---
+++ DWA10 Github/SRC/JAVA/script.js
@@ -0,0 +1,46 @@
+const MAX_NUMBER = 10
+const MIN_NUMBER = -10
+const STEP_AMOUNT = 1
+
+const currentNumber = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
+const reset = document.querySelector('[data-key="reset"]')
+
+const subtractHandler = () => {
+    const newValue = parseInt(currentNumber.value) - STEP_AMOUNT
+    currentNumber.value = newValue
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
+
+}
+const addHandler = () => {
+    const newValue = parseInt(currentNumber.value) + STEP_AMOUNT
+    currentNumber.value = newValue
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+}
+
+const resetHandler = () => {
+
+    if (currentNumber.value < 0 || currentNumber.value > 0) {
+        currentNumber.value = 0
+        setTimeout(function() {
+            alert("counter has been reset");
+        },10)
+    }
+}
+
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
+reset.addEventListener('click', resetHandler)
